61b792684f326ed1476aa8c228db3828fa8592e0
MuhammadAulia95 <adeaca777@gmail.com>
2021-08-21 14:34:19 +0700

    menambahkan file main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+print('Daftar nama buah :')
 namaBuah = 'apel', 'mangga', 'pepaya'
 
 for urut in namaBuah:
     print(urut)
+
+print('Daftar nama hewan :')
+namaHewan = 'beruang', 'singa', 'monyet'
+
+for hewan in namaHewan:
+    print(hewan)
